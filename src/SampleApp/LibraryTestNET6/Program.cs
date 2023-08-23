8586f74ac7ec9d59d9a05ae064157c724396965d
= <=>
2023-08-23 09:28:04 -0300

    feat: initial version

---
+++ src/SampleApp/LibraryTestNET6/Program.cs
@@ -0,0 +1,15 @@
+﻿using PIDevGuru.PIWebApiClient;
+using PIDevGuru.PIWebApiClient.Models;
+
+namespace LibraryTestNET6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PIWebAPI client = new PIWebAPI("https://marc-pi2018.marc.net/piwebapi/");
+            client.SetBasicAuth("marc.adm", "1");
+            PWADataServer dataServer = client.DataServer.GetByPath("\\\\MARC-PI2018");
+        }
+    }
+}
---
+++ src/SampleApp/LibraryTestNETCore21/Program.cs
@@ -0,0 +1,16 @@
+﻿using PIDevGuru.PIWebApiClient.Models;
+using PIDevGuru.PIWebApiClient;
+using System;
+
+namespace LibraryTestNETCore21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PIWebAPI client = new PIWebAPI("https://marc-pi2018.marc.net/piwebapi/");
+            client.SetBasicAuth("marc.adm", "1");
+            PWADataServer dataServer = client.DataServer.GetByPath("\\\\MARC-PI2018");
+        }
+    }
+}
